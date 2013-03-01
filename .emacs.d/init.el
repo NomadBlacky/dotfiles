f77d5d6668c282767d6d41dba0f01083d379387e
hourou.hamurinzu@gmail.com <hourou.hamurinzu@gmail.com>
2013-03-02 01:39:45 +0900

    20130302

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -25,5 +25,49 @@
 ;;
 ;; Lispコメント用マクロ
 ;;
-(fset 'comment-lisp
+(fset 'cmt-lisp
    [?\; ?\; return ?\; ?\; return ?\; ?\; up ? ])
+
+;;
+;; C-mにnewline-and-indentを割り当てる。初期値はnewline
+;;
+(define-key global-map (kbd "C-m") 'newline-and-indent)
+;; (global-set-key (kbd "C-m") 'newline-and-indent)
+
+;;
+;; 折り返しトグルコマンド
+;;
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;;
+;; "C-t"でウィンドウを切り替える。初期値はtranspose-chars
+;;
+(define-key global-map (kbd "C-t") 'other-window)
+
+;;
+;; カラム番号を表示
+;;
+(column-number-mode t)
+
+;;
+;; ファイルサイズを表示
+;;
+;;(size-indication-mode t)
+
+;;
+;; 時間を表示
+;;
+;;(setq display-time-day-and-date t) ;曜日・月・日を表示
+;;(setq display-time-24hr-format t) ;24時間表示
+;;(display-time-mode t)
+
+;;
+;; タイトルバーにファイルのフルパスを表示
+;;
+(setq frame-title-format "%f")
+
+;;
+;; TAB幅の設定。初期値は8。
+;;
+(setq-default tab-width 4)
+
